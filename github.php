7e51a57bb38c6bb53557c0234b9459628452f443
Neil Hainsworth <howdy@neilorangepeel.com>
2017-02-11 12:00:48 +0000

    adding notes

--- github.php
+++ github.php
@@ -1,3 +1,7 @@
 <?php
-	`git pull`;
+/**
+ * The webhook from github to pull changes from development to live.
+ *
+ * @see https://code.tutsplus.com/tutorials/the-perfect-workflow-with-git-github-and-ssh--net-19564 by Jeffery Way
+ */
 ?>
